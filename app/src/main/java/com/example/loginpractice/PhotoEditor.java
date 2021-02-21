dc7a61f39da7d9d911196b8584b4c410880909b5
Hunter Cordon <hccordon@student.fullsail.edu>
2021-02-21 17:42:02 -0500

    commit week 3

--- app/src/main/java/com/example/loginpractice/PhotoEditor.java
+++ app/src/main/java/com/example/loginpractice/PhotoEditor.java
@@ -1,31 +1,167 @@
 package com.example.loginpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 public class PhotoEditor extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 1;
     private Button eFromGallery;
     private Button eFromCamera;
+    Uri imageUri;
+    private ImageView eBackButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_editor);
         eFromGallery = findViewById((R.id. btnGallery));
         eFromCamera = findViewById((R.id.btnFromCamera));
+        eBackButton = findViewById((R.id. ivBackButton));
+        //request for camera permission
+        if (ContextCompat.checkSelfPermission(PhotoEditor.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(PhotoEditor.this, new String[]{
+                    Manifest.permission.CAMERA
+            },
+                    100);
+        }
+        eBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhotoEditor.this, HomePage.class);
+                startActivity(intent);
+
+            }
+
+        });
 
 
+        eFromGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
 
-        //Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
+                startActivityForResult(Intent.createChooser((gallery), "select Picture"), PICK_IMAGE);
 
-        //dsPhotoEditorIntent.setData(inputImageUri);
+            }
+
+        });
+
+        eFromCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open Camera
+                Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+
+            }
+
+        });
 
     }
-}
+
+
+       /*
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data)   {
+        if (requestCode == 100) {
+            Bitmap captureImage = (Bitmap)  data.getExtras().get("data");
+
+        }
+        }
+*/
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data)   {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+
+            Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
+
+            dsPhotoEditorIntent.setData(imageUri);
+
+            dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "YOUR_OUTPUT_IMAGE_FOLDER");
+            startActivityForResult(dsPhotoEditorIntent, 200);
+        }
+
+        }
+
+      /*public Uri bitmapToUriConverter(Bitmap mBitmap) {
+         Uri uri = null;
+         try {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            // Calculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, 100, 100);
+
+            // Decode bitmap with inSampleSize set
+            options.inJustDecodeBounds = false;
+            Bitmap newBitmap = Bitmap.createScaledBitmap(mBitmap, 200, 200,
+                    true);
+            File file = new File(getActivity().getFilesDir(), "Image"
+                    + new Random().nextInt() + ".jpeg");
+            FileOutputStream out = getActivity().openFileOutput(file.getName(),
+                    Context.MODE_WORLD_READABLE);
+            newBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            //get absolute path
+            String realPath = file.getAbsolutePath();
+            File f = new File(realPath);
+            uri = Uri.fromFile(f);
+
+        } catch (Exception e) {
+            Log.e("Your Error Message", e.getMessage());
+        }
+        return uri;
+    }
+    */
+
+    /*public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+    return inSampleSize;
+    }
+   */
+    }
