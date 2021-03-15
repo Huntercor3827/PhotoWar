76ff17bea83392c1fb18770286144ef02c91167e
Hunter Cordon <hccordon@student.fullsail.edu>
2021-03-15 17:15:35 -0400

    Week 6

---
+++ app/src/main/java/com/example/loginpractice/PCInvite.java
@@ -0,0 +1,14 @@
+package com.example.loginpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PCInvite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_c_invite);
+    }
+}
